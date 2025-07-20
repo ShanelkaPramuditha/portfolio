b159994298e0ca826d03cd79112a7e335310f407
Shanelka Pramuditha <99pramuditha@gmail.com>
2025-07-20 14:05:57 +0530

    feat: add .prettierignore file and update package.json for formatting script

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,11 +2,9 @@ export default async function Page() {
   const data = await fetch('https://api.vercel.app/blog');
   const posts = await data.json();
 
-  console.log(posts);
-
   return (
     <ul>
-      {posts.map((post) => (
+      {posts.map((post: any) => (
         <li key={post.id}>{post.title}</li>
       ))}
     </ul>
