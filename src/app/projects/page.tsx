1bf1d773694d7decea0759fe48a35c1964b269a0
Shanelka Pramuditha <99pramuditha@gmail.com>
2025-07-20 14:08:17 +0530

    refactor: comment out data fetching logic in projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,12 +1,12 @@
 export default async function Page() {
-  const data = await fetch('https://api.vercel.app/blog');
-  const posts = await data.json();
+  // const data = await fetch('https://api.vercel.app/blog');
+  // const posts = await data.json();
 
   return (
     <ul>
-      {posts.map((post: any) => (
+      {/* {posts.map((post: any) => (
         <li key={post.id}>{post.title}</li>
-      ))}
+      ))} */}
     </ul>
   );
 }
